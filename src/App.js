c47f5497c0caafb3f347e5910dd8df9ac761ec41
saisrinivaslakkakula <38246768+saisrinivaslakkakula@users.noreply.github.com>
2021-06-15 20:25:04 -0700

    deploy to github

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello There,</h1>
+      <h3>Welcome to my portfolio website. The website is undergoing Maintenance activity and will be live soon.</h3>
+      <p> In the Meantime, Please feel free to visit my <a href="https://www.linkedin.com/in/sai-srinivas-lakkakula-344317bb/">LinkedIn</a> profile</p>
+    </div>
+  );
+}
+
+export default App;
